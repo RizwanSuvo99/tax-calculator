310268d0bfe22bbf55859deffb8b47108bf22fbf
RizwanSuvo99 <rizwanshuvo@gmail.com>
2023-10-01 17:25:25 +0600

    slight changes in tax return line

--- main.js
+++ main.js
@@ -12,24 +12,24 @@ const resetBtn = document.querySelector("#resetBtn");
 const input = document.querySelector("#form-input");
 const output = document.querySelector(".output");
 
-btn.addEventListener("click",()=>{
-    const inputValue = Number(input.value);
-    if(inputValue <= 0 || isNaN(inputValue)){
-        return output.innerHTML = `<div class="alert alert-danger" role="alert">
+btn.addEventListener("click", () => {
+  const inputValue = Number(input.value);
+  if (inputValue <= 0 || isNaN(inputValue)) {
+    return (output.innerHTML = `<div class="alert alert-danger" role="alert">
         Please Enter a Number greater than Zero
-      </div>`;
-    }
-    
-    const tax = taxCalculator(inputValue);
-    output.innerHTML = `<p class="border text-center py-3 fs-2 shadow-md shadow">Your tax is $${tax}</p>`
-})
+      </div>`);
+  }
 
-resetBtn.addEventListener("click",()=>{
-    return output.innerHTML = "&nbsp;";
-})
+  const tax = taxCalculator(inputValue);
+  output.innerHTML = `<p class="border text-center py-3 fs-2 shadow-md shadow">Your tax is $${tax}</p>`;
+});
+
+resetBtn.addEventListener("click", () => {
+  return (output.innerHTML = "&nbsp;");
+});
 
 function taxCalc(income, taxParentage) {
-  tax = (income * taxParentage / 100);
+  tax = (income * taxParentage) / 100;
   return tax;
 }
 
@@ -42,29 +42,27 @@ function taxCalculator(num) {
     { income: 20000, taxParentage: 25 },
   ];
   let maxPertng = 30;
-  let amount = num - taxRateByIncome[0].income;//4000
+  let amount = num - taxRateByIncome[0].income; //3000 - 3000 = 0
 
-  for(let i = 0; i < taxRateByIncome.length - 1; i++){
-    if(amount <= 0) break;
+  for (let i = 0; i < taxRateByIncome.length - 1; i++) {
+    if (amount <= 0) break;
     let curSlab = taxRateByIncome[i + 1].income - taxRateByIncome[i].income;
     let pertg = taxRateByIncome[i + 1].taxParentage;
-    if(amount > curSlab){
-        curSlab = curSlab;
+    if (amount > curSlab) {
+      curSlab = curSlab;
+    } else {
+      curSlab = amount;
     }
-    else{
-        curSlab = amount;
-    }
-    tax += taxCalc(curSlab,pertg);
+    tax += taxCalc(curSlab, pertg);
     amount -= curSlab;
-    if(i == taxRateByIncome.length - 2 && amount > 0){
-        console.log(amount);
-        tax += taxCalc(amount,maxPertng);
+    if (i == taxRateByIncome.length - 2 && amount > 0) {
+      console.log(amount);
+      tax += taxCalc(amount, maxPertng);
     }
   }
-  return tax < 50 ? tax == 0 ? 0 : 50 : tax;
+
+  return tax < 50 ? (tax <= 0 ? 0 : 50) : tax;
 }
-
-
 
 // ------------------------------------------------------------------------
 // travel allowance -> 500 USD without tax; above that -> add with income
